aec1b3179b3d51665ca1eabf469376c6ec4b0bec
Bulelani Nhlanhla <bulelani.kafu@outlook.com>
2022-11-29 15:38:07 +0200

    Extra files
    base.html added to templates and python code change

--- capp.py
+++ capp.py
@@ -10,17 +10,14 @@ from flask import Flask, render_template
 
 
 
-# Create a Flask Instance
-app = Flask(__name__)
+
+
 
 
 #Create a route decorrator
 @app.route('/')
-
-#def index():
-       #return "Hello world"
-
 def index():
+    #return "Hello world"
     #return render_template("index.html")
     fav_car = "BMW"
     cars = ["Ford", "Volvo", "BMW", 20000, 431413]
@@ -29,7 +26,6 @@ def index():
 
 # localhost:5000/user/Kafu
 @app.route('/user/<name>')  
-
 def user(name):
     #return "Hello {}".format(name)
     return render_template("user.html", user_name=name)
@@ -45,3 +41,5 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500    
+
+
