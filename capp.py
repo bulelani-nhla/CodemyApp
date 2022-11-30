51ec9dd237deb860db42aef061d3efe362858fde
Bulelani Nhlanhla <bulelani.kafu@outlook.com>
2022-11-30 15:27:57 +0200

    code uopdates
    code is updated and the extra html files  uploaded

--- capp.py
+++ capp.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 # FILTERS !!!!!
 # capitalize
@@ -11,7 +14,66 @@ from flask import Flask, render_template
 
 # Create a Flask Instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "R3EWTG BWBE NYTJT JM YEYR "
 
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What is your name ?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+    # BooleanField
+    # DataField
+    # DataTimeField
+    # DecimalField
+    # FileField
+    # HiddenField
+    # MultipleField
+    # FieldList
+    # FloatField
+    # FormField
+    # IntergerField
+    # PasswordField
+    # RadioField
+    # SelectField
+    # SelectMultipleField
+    # StringField
+    # TextAreaField
+
+    ## Validators
+    # DataRequired
+    # Email
+    # EqualTo
+    # InputRequired
+    # IPAddress
+    # MacAddress
+    # Length
+    # NumberRange
+    # Optional
+    # Regexp
+    # URL
+    # UUID
+    # AnyOf
+    # NoneOf
+
+
+
+
+
+
+
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+# Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully !!!")
+    return render_template("name.html", name = name, form = form)
 
 
 #Create a route decorrator
@@ -30,6 +92,11 @@ def user(name):
     #return "Hello {}".format(name)
     return render_template("user.html", user_name=name)
 
+
+
+
+
+
 # Create Custom Error Pages
 
 # Invalid URL
@@ -41,5 +108,3 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html"), 500    
-
-
